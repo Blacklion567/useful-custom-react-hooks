cbb3de96bc94fd637228c9e89bca7e81bf082075
Blacklion567 <naviedaj567@gmail.com>
2023-11-20 19:25:02 +0800

    3-useDebounce

---
+++ src/3-useDebounce/DebounceComponent.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import useDebounce from "./useDebounce";
+
+
+export default function DebounceComponent() {
+  const [count, setCount] = useState(10);
+  useDebounce(() => alert(count), 1000, [count]);
+
+
+  return (
+    <div>
+      <div> {count}</div>
+
+      <button
+        onClick={() => setCount(prev => prev + 1)}
+      >Increment</button>
+    </div>
+  );
+
+}
