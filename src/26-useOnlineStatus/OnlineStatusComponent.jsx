53486505f5c2ac798e8243a57cf0c59c2206c329
Blacklion567 <naviedaj567@gmail.com>
2024-01-02 10:03:35 +0800

    26-useOnlineStatus

---
+++ src/26-useOnlineStatus/OnlineStatusComponent.jsx
@@ -0,0 +1,9 @@
+import useOnlineStatus from "./useOnlineStatus";
+
+const OnlineStatusComponent = () => {
+  const online = useOnlineStatus();
+
+  return <div>{online.toString()}</div>;
+};
+
+export default OnlineStatusComponent;
---
+++ src/26-useOnlineStatus/useOnlineStatus.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import useEventListener from '../13-useEventListener/useEventListener';
+
+
+const useOnlineStatus = () => {
+  const [online, setOnline] = useState(navigator.onLine);
+
+  useEventListener('online', () => setOnline(navigator.onLine));
+  useEventListener('offline', () => setOnline(navigator.onLine));
+
+  return online;
+};
+
+export default useOnlineStatus;
