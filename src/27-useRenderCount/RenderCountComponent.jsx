da02d8987457c8c973aaa61d9674cf7d65933018
Blacklion567 <naviedaj567@gmail.com>
2024-01-03 10:38:56 +0800

    27-useRenderCount

---
+++ src/27-useRenderCount/RenderCountComponent.jsx
@@ -0,0 +1,20 @@
+import useRenderCount from "./useRenderCount";
+import useToggle from '../01-useToggle/useToggle';
+
+const RenderCountComponent = () => {
+  const [boolean, toggle] = useToggle(false);
+
+  const renderCount = useRenderCount;
+
+  return (
+    <>
+      <div>{boolean.toString()}</div>
+      <div>{renderCount}</div>
+      <button onClick={toggle}>Toggle</button>
+    </>
+  );
+
+};
+
+
+export default RenderCountComponent;
